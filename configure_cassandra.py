63972448f4b3bfdfacc6c284d29bbcac40ee3660
Brian O'Neill <bone@alumni.brown.edu>
2015-07-08 15:05:19 -0400

    FQ path to template

--- configure_cassandra.py
+++ configure_cassandra.py
@@ -5,7 +5,7 @@ import io
 config = io.open(sys.argv[1], 'w')
 
 # Read the lines from the template, substitute the values, and write to the new config file
-for line in io.open('cassandra.yaml.template', 'r'):
+for line in io.open('/home/ec2-user/cassandra.yaml.template', 'r'):
     line = line.replace('$cluster_name', sys.argv[2])
     line = line.replace('$cassandra_seeds', sys.argv[3])
     config.write(line)
